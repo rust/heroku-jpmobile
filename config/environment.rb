902ab906bef7e84119a678d9f525bb3a7ee89f47
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2010-07-01 18:51:54 +0900

    bundle package
    Signed-off-by: Shin-ichiro OGAWA <rust.stnard+git@gmail.com>

--- config/environment.rb
+++ config/environment.rb
@@ -3,11 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 JpmobileTest::Application.initialize!
-
-eval %Q{ 
-  class ActiveSupport::Multibyte::Chars
-    def <=>(others)
-      0
-    end
-  end
-}
