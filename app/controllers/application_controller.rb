0f3521d48a4fd00db4dad6898fa0861dd157c394
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2010-07-02 15:34:37 +0900

    remove jpmobile, and change output encoding to Shift_JIS
    Signed-off-by: Shin-ichiro OGAWA <rust.stnard+git@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
+
+  after_filter :convert_to_sjis
+
+  def convert_to_sjis
+    body = response.body
+    body = NKF.nkf("-sWx -m0", body)
+    response.body = body
+    response.charset = "Shift_JIS"
+  end
 end
