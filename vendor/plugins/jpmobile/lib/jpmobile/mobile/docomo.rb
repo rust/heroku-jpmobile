d556c28ef4b9b0277a11550fc20ca8ad336156cb
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2010-07-05 18:16:24 +0900

    debug
    Signed-off-by: Shin-ichiro OGAWA <rust.stnard+git@gmail.com>

--- vendor/plugins/jpmobile/lib/jpmobile/mobile/docomo.rb
+++ vendor/plugins/jpmobile/lib/jpmobile/mobile/docomo.rb
@@ -83,11 +83,12 @@ module Jpmobile::Mobile
     # 文字コード変換
     def to_internal(str)
       # 絵文字を数値参照に変換
-      str = Jpmobile::Emoticon.external_to_unicodecr_docomo(Jpmobile::Util.sjis(str))
+puts      str = Jpmobile::Emoticon.external_to_unicodecr_docomo(Jpmobile::Util.sjis(str))
       # 文字コードを UTF-8 に変換
-      str = Jpmobile::Util.sjis_to_utf8(str)
+puts      str = Jpmobile::Util.sjis_to_utf8(str)
       # 数値参照を UTF-8 に変換
-      Jpmobile::Emoticon.unicodecr_to_utf8(str)
+puts      str = Jpmobile::Emoticon.unicodecr_to_utf8(str)
+      str
     end
     def to_external(str, content_type, charset)
       # UTF-8を数値参照に
